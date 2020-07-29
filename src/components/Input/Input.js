6c9407f8c8342975b75d1aa34bf5cc2f47abe58b
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-07-29 19:51:47 +0545

    Minor design refactor

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,13 +3,14 @@ import { FormControl, InputLabel, Input } from "@material-ui/core";
 
 function InputFeild(props) {
   return (
-    <FormControl>
+    <FormControl className='app-form-control'>
       <InputLabel htmlFor="my-input">Enter a message...</InputLabel>
       <Input
         type={props.type}
         value={props.value}
         onChange={props.onChange}
         id="my-input"
+        autoComplete="off"
         aria-describedby="my-helper-text"
       />
     </FormControl>
