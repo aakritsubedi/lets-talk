1da4e1134170c14bd3ef0671cd74a9502e8cc4f1
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-07-29 19:51:17 +0545

    Fetch User Ip Info

--- src/components/ChatApp/ChatApp.js
+++ src/components/ChatApp/ChatApp.js
@@ -12,16 +12,24 @@ function ChatApp() {
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState([]);
   const [username, setUsername] = useState("");
+  const [userInfo, setUserInfo] = useState({})
 
   useEffect(() => {
     setUsername(prompt("Please enter your name"));
   }, []);
 
+  useEffect(async () => {
+    const userIpInfo = await fetch('https://ipapi.co/json').then(res => res.json());
+    setUserInfo(userIpInfo);
+  }, [])
+
   useEffect(() => {
     db.collection("messages")
-      .orderBy("timestamp", "asc")
+      .orderBy("timestamp", "desc")
       .onSnapshot((snapshot) => {
-        setMessages(snapshot.docs.map((doc) => ({id: doc.id, msg: doc.data()})));
+        setMessages(
+          snapshot.docs.map((doc) => ({ id: doc.id, msg: doc.data() }))
+        );
       });
   }, []);
 
@@ -33,6 +41,10 @@ function ChatApp() {
     db.collection("messages").add({
       username: username,
       message: input,
+      ip: userInfo.ip,
+      city: userInfo.city,
+      region: userInfo.region,
+      org: userInfo.org,
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
     });
     setInput("");
@@ -41,16 +53,18 @@ function ChatApp() {
   return (
     <>
       <h1>Let's Talk</h1>
-      <form>
+      <form className="app-form">
         <Input type="text" onChange={changeInput} value={input} />
         <Button type="submit" sendMsg={sendMessage} disabled={!input} />
       </form>
 
-      <FlipMove>
-        {messages.map(({id, msg}) => (
-          <MessageBox message={msg} key={id} username={username} />
-        ))}
-      </FlipMove>
+      <div className='message-wrapper'>
+        <FlipMove>
+          {messages.map(({ id, msg }) => (
+            <MessageBox message={msg} key={id} username={username}/>
+          ))}
+        </FlipMove>
+      </div>
     </>
   );
 }
