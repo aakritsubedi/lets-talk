a71f8a26275f8bee7bf9cb8e1d7ebe06a353664f
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-07-29 17:53:17 +0545

    ChatApp

--- src/components/ChatApp/ChatApp.js
+++ src/components/ChatApp/ChatApp.js
@@ -1,4 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import firebase from "firebase";
+import FlipMove from "react-flip-move";
+
+import db from "firebase.js";
 
 import Input from "components/Input";
 import Button from "components/Button";
@@ -7,13 +11,30 @@ import MessageBox from "components/MessageBox";
 function ChatApp() {
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    setUsername(prompt("Please enter your name"));
+  }, []);
+
+  useEffect(() => {
+    db.collection("messages")
+      .orderBy("timestamp", "asc")
+      .onSnapshot((snapshot) => {
+        setMessages(snapshot.docs.map((doc) => ({id: doc.id, msg: doc.data()})));
+      });
+  }, []);
 
   let changeInput = (e) => {
     setInput(e.target.value);
   };
   let sendMessage = (e) => {
     e.preventDefault();
-    setMessages([...messages, input]);
+    db.collection("messages").add({
+      username: username,
+      message: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
     setInput("");
   };
 
@@ -22,12 +43,14 @@ function ChatApp() {
       <h1>Let's Talk</h1>
       <form>
         <Input type="text" onChange={changeInput} value={input} />
-        <Button type="submit" sendMsg={sendMessage} />
+        <Button type="submit" sendMsg={sendMessage} disabled={!input} />
       </form>
 
-      {messages.map((msg, index) => (
-        <MessageBox msg={msg} key={index} />
-      ))}
+      <FlipMove>
+        {messages.map(({id, msg}) => (
+          <MessageBox message={msg} key={id} username={username} />
+        ))}
+      </FlipMove>
     </>
   );
 }
